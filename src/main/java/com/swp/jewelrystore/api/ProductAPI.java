c070208b24b7017bdf0f83762adb8d0ac00922a3
phucphse181514@fpt.edu.vn <phucphse181514@fpt.edu.vn>
2024-06-08 16:52:34 +0700

    update project

--- src/main/java/com/swp/jewelrystore/api/ProductAPI.java
+++ src/main/java/com/swp/jewelrystore/api/ProductAPI.java
@@ -22,7 +22,7 @@ public class ProductAPI {
             @ApiImplicitParam(name = "category_name", value = "Trang sức", dataType = "string", paramType = "query"),
             @ApiImplicitParam(name = "product_name", value = "Nhẫn Kim cương Vàng trắng 14K",dataType = "string", paramType = "query"),
             @ApiImplicitParam(name = "product_code", value = "NKCVT14K", dataType = "string", paramType = "query"),
-            @ApiImplicitParam(name = "invoiceCode", value = "SELL1", dataType = "string", paramType = "query")
+            @ApiImplicitParam(name = "invoiceCode", value = "SELL1 or PURC1", dataType = "string", paramType = "query")
     })
     @GetMapping
     public List<ProductResponseDTO> getListProduct(@RequestParam Map <String, String> params ){
