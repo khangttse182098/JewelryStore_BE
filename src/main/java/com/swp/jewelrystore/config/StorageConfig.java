c79f564d6572ea1fedc962c86de2c11af88f4228
phucphse181514@fpt.edu.vn <phucphse181514@fpt.edu.vn>
2024-06-09 22:48:12 +0700

    feat: uploade image function

--- src/main/java/com/swp/jewelrystore/config/StorageConfig.java
+++ src/main/java/com/swp/jewelrystore/config/StorageConfig.java
@@ -10,7 +10,6 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 @Configuration
 public class StorageConfig {
 
