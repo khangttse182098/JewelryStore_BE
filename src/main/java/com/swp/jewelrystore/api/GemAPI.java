b9bee962bc61fc30d93a606a15610c5b04840ac8
phucphse181514@fpt.edu.vn <phucphse181514@fpt.edu.vn>
2024-06-18 15:38:37 +0700

    feat: return gem list

--- src/main/java/com/swp/jewelrystore/api/GemAPI.java
+++ src/main/java/com/swp/jewelrystore/api/GemAPI.java
@@ -1,12 +1,26 @@
 package com.swp.jewelrystore.api;
 
+import com.swp.jewelrystore.model.response.GemResponseDTO;
+import com.swp.jewelrystore.service.IGemService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/api/gem")
 public class GemAPI {
-    
+    @Autowired
+    private IGemService iGemService;
+
+    @GetMapping
+    public List<GemResponseDTO> getGem() {
+        List<GemResponseDTO> gemResponseDTOS = iGemService.getAllGem();
+        return gemResponseDTOS;
+    }
 }
--- src/main/java/com/swp/jewelrystore/service/impl/GemService.java
+++ src/main/java/com/swp/jewelrystore/service/impl/GemService.java
@@ -4,20 +4,28 @@ import com.swp.jewelrystore.entity.GemEntity;
 import com.swp.jewelrystore.model.response.GemResponseDTO;
 import com.swp.jewelrystore.repository.GemRepository;
 import com.swp.jewelrystore.service.IGemService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 @Service
 public class GemService implements IGemService {
     @Autowired
     private GemRepository gemRepository;
+    @Autowired
+    private ModelMapper modelMapper;
 
     @Override
     public List<GemResponseDTO> getAllGem() {
+        List<GemResponseDTO> gemResponseDTOS = new ArrayList<>();
         List<GemEntity> gemEntities = gemRepository.findGemEntityByActive(1L);
-
-        return Collections.emptyList();
+        for (GemEntity gemEntity : gemEntities) {
+            GemResponseDTO gemResponseDTO = modelMapper.map(gemEntity, GemResponseDTO.class);
+            gemResponseDTOS.add(gemResponseDTO);
+        }
+        return gemResponseDTOS;
     }
 }
