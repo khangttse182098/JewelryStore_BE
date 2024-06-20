c8623badd77d27f442d3fbdec0e30b50dc50f272
khanghnse181562@fpt.edu.vn <khanghnse181562@fpt.edu.vn>
2024-06-20 16:02:22 +0700

    fix: fix some stuff

--- src/main/java/com/swp/jewelrystore/converter/UserStatusConverter.java
+++ src/main/java/com/swp/jewelrystore/converter/UserStatusConverter.java
@@ -3,14 +3,14 @@ package com.swp.jewelrystore.converter;
 import com.swp.jewelrystore.constant.SystemConstant;
 
 public class UserStatusConverter {
-    public static String convertNumberToText(Long number) {
+    public static String convertStatusFromNumberToText(Long number) {
          if (number == 1) {
              return SystemConstant.ON_STATUS;
          }
          return SystemConstant.OFF_STATUS;
     }
 
-    public static Long convertTextToNumber(String text) {
+    public static Long convertStatusFromTextToNumber(String text) {
         if (text.equals(SystemConstant.ON_STATUS)) {
             return 1L;
         }
