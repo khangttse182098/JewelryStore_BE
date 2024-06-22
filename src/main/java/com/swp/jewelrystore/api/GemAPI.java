e6effd8c651553e7b5cc357ea0a1609bb820cff9
khanghnse181562@fpt.edu.vn <khanghnse181562@fpt.edu.vn>
2024-06-23 00:36:59 +0700

    fix: use diamond-price for this feature

--- src/main/java/com/swp/jewelrystore/api/GemAPI.java
+++ src/main/java/com/swp/jewelrystore/api/GemAPI.java
@@ -1,14 +1,9 @@
 package com.swp.jewelrystore.api;
 
 
-import com.swp.jewelrystore.constant.SystemConstant;
-import com.swp.jewelrystore.model.dto.GemWithPriceDTO;
-import com.swp.jewelrystore.model.response.GemDetailResponseDTO;
 import com.swp.jewelrystore.model.response.GemResponseDTO;
-import com.swp.jewelrystore.model.response.ResponseDTO;
 import com.swp.jewelrystore.service.IGemService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -27,22 +22,4 @@ public class GemAPI {
         return iGemService.getAllGem();
     }
 
-    @GetMapping("/information-{id}")
-    public GemDetailResponseDTO getGemDetail(@PathVariable Long id) {
-        return iGemService.getGemDetail(id);
-    }
-
-    @PostMapping("/add")
-    public ResponseEntity<ResponseDTO> addOrUpdateGemWithPrice(@RequestBody GemWithPriceDTO gem){
-        ResponseDTO responseDTO = new ResponseDTO();
-        try {
-            iGemService.addOrUpdateGemWithPrice(gem);
-            responseDTO.setMessage(SystemConstant.ADD_OR_UPDATE_DIAMOND_SUCCESSFULLY);
-            responseDTO.setData(gem);
-            return ResponseEntity.ok(responseDTO);
-        }  catch (Exception e) {
-            responseDTO.setMessage(e.toString());
-            return ResponseEntity.badRequest().body(responseDTO);
-        }
-    }
 }
--- src/main/java/com/swp/jewelrystore/service/impl/GemService.java
+++ src/main/java/com/swp/jewelrystore/service/impl/GemService.java
@@ -3,16 +3,12 @@ package com.swp.jewelrystore.service.impl;
 import com.swp.jewelrystore.converter.DateTimeConverter;
 import com.swp.jewelrystore.entity.GemEntity;
 import com.swp.jewelrystore.entity.GemPriceEntity;
-import com.swp.jewelrystore.model.dto.DiamondCriteriaDTO;
-import com.swp.jewelrystore.model.dto.GemWithPriceDTO;
-import com.swp.jewelrystore.model.response.GemDetailResponseDTO;
 import com.swp.jewelrystore.model.response.GemResponseDTO;
 import com.swp.jewelrystore.repository.GemPriceRepository;
 import com.swp.jewelrystore.repository.GemRepository;
 import com.swp.jewelrystore.service.IGemService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.PropertyMap;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -40,40 +36,4 @@ public class GemService implements IGemService {
         return gemResponseDTOS;
     }
 
-    @Override
-    public GemDetailResponseDTO getGemDetail(Long id) {
-        GemEntity gemEntity = gemRepository.findGemEntityById(id);
-        GemDetailResponseDTO gemDetail = modelMapper.map(gemEntity, GemDetailResponseDTO.class);
-        GemPriceEntity gemPrice = gemPriceRepository.findLatestGemPrice(gemEntity);
-        gemDetail.setEffectDate(dateTimeConverter.convertToDateTimeResponse(gemPrice.getEffectDate()));
-        gemDetail.setBuyPrice(gemPrice.getBuyPrice());
-        gemDetail.setSellPrice(gemPrice.getSellPrice());
-        return gemDetail;
-    }
-
-    @Override
-    public void addOrUpdateGemWithPrice(GemWithPriceDTO gem) {
-        GemEntity gemEntity = gemRepository.findById(gem.getGemId()).orElse(null);
-        // using condition in modelMapper to avoid id
-        PropertyMap<GemPriceEntity, GemPriceEntity> gemPriceMap = new PropertyMap<GemPriceEntity, GemPriceEntity>() {
-            protected void configure() {
-                skip(destination.getId());
-            }
-        };
-         modelMapper.addMappings(gemPriceMap);
-         if (gemEntity == null) {
-
-         } else {
-             // change diamond name
-             gemEntity.setGemName(gem.getDiamondName());
-             gemRepository.save(gemEntity);
-             DiamondCriteriaDTO diamondCriteria = modelMapper.map(gemEntity, DiamondCriteriaDTO.class);
-             GemPriceEntity gemPriceEntity = gemPriceRepository.checkGemCaratInRange(diamondCriteria);
-             GemPriceEntity newRecordForGem = modelMapper.map(gemPriceEntity, GemPriceEntity.class);
-             newRecordForGem.setBuyPrice(gem.getBuyPrice());
-             newRecordForGem.setSellPrice(gem.getSellPrice());
-             newRecordForGem.setEffectDate(dateTimeConverter.convertToDateTimeDTO(gem.getEffectDate()));
-             gemPriceRepository.save(newRecordForGem);
-         }
-    }
 }
