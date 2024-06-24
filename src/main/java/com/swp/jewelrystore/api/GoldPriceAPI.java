ae1da920d876a8bad4cdea5953bfa363b0c87e00
phucphse181514@fpt.edu.vn <phucphse181514@fpt.edu.vn>
2024-06-24 13:04:47 +0700

    update main

--- src/main/java/com/swp/jewelrystore/api/GoldPriceAPI.java
+++ src/main/java/com/swp/jewelrystore/api/GoldPriceAPI.java
@@ -59,12 +59,16 @@ public class GoldPriceAPI {
     @PostMapping
     public ResponseEntity<ResponseDTO> addOrUpdateGoldPrice(@Valid @RequestBody MaterialPriceDTO materialPriceDTO){
         ResponseDTO responseDTO = new ResponseDTO();
-        materialPriceService.addOrUpdateMaterialPrice(materialPriceDTO);
-        if(materialPriceDTO.getMaterialId() != null){
-            responseDTO.setMessage("Update material price successfully");
+        try {
+            materialPriceService.addOrUpdateMaterialPrice(materialPriceDTO);
+            if(materialPriceDTO.getMaterialId() != null){
+                responseDTO.setMessage("Update material price successfully");
+            }
+            responseDTO.setMessage("Add marterial price successfully");
             return ResponseEntity.ok(responseDTO);
+        } catch (Exception e){
+            responseDTO.setMessage(e.toString());
+            return ResponseEntity.badRequest().body(responseDTO);
         }
-        responseDTO.setMessage("Add marterial price successfully");
-        return ResponseEntity.ok(responseDTO);
     }
 }
