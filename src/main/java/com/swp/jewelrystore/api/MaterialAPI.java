4085ca81249d3fa3e1b69431f670d2f59f79618c
phucphse181514@fpt.edu.vn <phucphse181514@fpt.edu.vn>
2024-07-08 21:16:20 +0700

    fix: update gold-price

--- src/main/java/com/swp/jewelrystore/api/MaterialAPI.java
+++ src/main/java/com/swp/jewelrystore/api/MaterialAPI.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping("/api/material")
+@RequestMapping("/api/material")    
 @CrossOrigin
 @RequiredArgsConstructor
 public class MaterialAPI {
